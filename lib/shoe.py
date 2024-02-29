9bda5bf2e1ea6cf4994dfe369f3c5ce53aed8979
Anna Kioko <annakioko00@gmail.com>
2024-02-29 21:29:27 +0300

    completed assignment

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size,int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    size = property(size,set_size)
+    
+
+    def condition(self):
+        return self.condition
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+    
+
+            
